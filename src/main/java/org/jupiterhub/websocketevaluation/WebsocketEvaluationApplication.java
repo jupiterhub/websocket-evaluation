5cec535d8f49e9afc3925683609b46a41043c1b0
Jupiter <4177229+jupiterhub@users.noreply.github.com>
2022-01-30 12:05:43 +0900

    build: initial commit

---
+++ src/main/java/org/jupiterhub/websocketevaluation/WebsocketEvaluationApplication.java
@@ -0,0 +1,13 @@
+package org.jupiterhub.websocketevaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketEvaluationApplication.class, args);
+	}
+
+}
